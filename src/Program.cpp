ecc86a3aa9e2a09e96279b8a40def0d41fc6fe8c
Wesley Jap-A-Joe <wjapajoe@ziggo.nl>
2020-12-05 17:34:28 -0300

    Reformatted Program.cpp

--- src/Program.cpp
+++ src/Program.cpp
@@ -7,7 +7,7 @@ std::vector<InspectorField> Program::inspectorFields;
 
 void Program::Initialize()
 {
-    SharpAudioAPI::onDebug = [this] (const std::string& message) { this->LogMessage(message); };
+    SharpAudioAPI::onDebug = [this](const std::string &message) { this->LogMessage(message); };
 
     boxV = std::make_shared<Box>(GtkOrientation::GTK_ORIENTATION_VERTICAL, 0, false);
     boxH = std::make_shared<Box>(GtkOrientation::GTK_ORIENTATION_HORIZONTAL, 0, false);
@@ -27,14 +27,14 @@ void Program::Initialize()
     labelInspector = std::make_shared<Label>("Inspector");
 
     InitializeMenu();
-    
+
     window->Add(boxV->widget);
-    boxV->Add(menubar->widget, false, false, 0);    
+    boxV->Add(menubar->widget, false, false, 0);
     boxV->Add(panel1->widget, false, false, 0);
     panel1->Add(drawingarea1->widget, 0, 0);
     panel1->Add(drawingarea2->widget, 134, 0);
     boxV->Add(splitcontainer1->widget, true, true, 0);
-    splitcontainer1->Add(splitcontainer2->widget, 0, true, false);    
+    splitcontainer1->Add(splitcontainer2->widget, 0, true, false);
     splitcontainer1->Add(scrolledInspector->widget, 1, false, false);
     scrolledInspector->Add(panel3->widget);
     splitcontainer2->Add(tabcontrol->widget, 0, true, false);
@@ -47,14 +47,14 @@ void Program::Initialize()
 
     auto sourceview = tabcontrol->AddItem(true, true);
     sourceview->button->SetText("Main");
-    
-    if(IO::FileExists("res/KeyboardWurley.cs"))
+
+    if (IO::FileExists("res/KeyboardWurley.cs"))
     {
         std::string source = IO::ReadAllText("res/KeyboardWurley.cs");
         sourceview->item->SetText(source);
     }
 
-    if(IO::FileExists("settings.bin"))
+    if (IO::FileExists("settings.bin"))
     {
         auto bytes = IO::ReadAllBytes("settings.bin");
         ByteStream stream(&bytes[0], bytes.size());
@@ -72,7 +72,7 @@ void Program::Initialize()
     drawingarea1->SetMargins(0, 0, 5, 0);
     drawingarea2->SetMargins(0, 0, 5, 0);
 
-    window->SetSize({ 800, 600});
+    window->SetSize({800, 600});
     window->SetTitle("SharpAudio");
 
     int height = window->rectangle.height;
@@ -88,25 +88,25 @@ void Program::Initialize()
 
 void Program::InitializeMenu()
 {
-    MenuInfo menuInfoFile("File");	
+    MenuInfo menuInfoFile("File");
     MenuInfo menuInfoEdit("Edit");
     MenuInfo menuInfoView("View");
     MenuInfo menuInfoMidi("Midi");
 
-    menuInfoFile.AddItem("Open",        [this] () { this->OnMenuItemOpenClicked(); });
-    menuInfoFile.AddItem("Save",        [this] () { this->OnMenuItemSaveClicked(); });
-    menuInfoFile.AddItem("Exit",        [this] () { this->OnMenuItemExitClicked(); });
+    menuInfoFile.AddItem("Open", [this]() { this->OnMenuItemOpenClicked(); });
+    menuInfoFile.AddItem("Save", [this]() { this->OnMenuItemSaveClicked(); });
+    menuInfoFile.AddItem("Exit", [this]() { this->OnMenuItemExitClicked(); });
 
-	menuInfoEdit.AddItem("Undo",        [this] () { this->OnMenuItemUndoClicked(); });
-	menuInfoEdit.AddItem("Redo",        [this] () { this->OnMenuItemRedoClicked(); });
-	menuInfoEdit.AddItem("Cut",         [this] () { this->OnMenuItemCutClicked(); });
-	menuInfoEdit.AddItem("Copy",        [this] () { this->OnMenuItemCopyClicked(); });
-	menuInfoEdit.AddItem("Paste",       [this] () { this->OnMenuItemPasteClicked(); });
-	menuInfoEdit.AddItem("Select All",  [this] () { this->OnMenuItemSelectAllClicked(); });
+    menuInfoEdit.AddItem("Undo", [this]() { this->OnMenuItemUndoClicked(); });
+    menuInfoEdit.AddItem("Redo", [this]() { this->OnMenuItemRedoClicked(); });
+    menuInfoEdit.AddItem("Cut", [this]() { this->OnMenuItemCutClicked(); });
+    menuInfoEdit.AddItem("Copy", [this]() { this->OnMenuItemCopyClicked(); });
+    menuInfoEdit.AddItem("Paste", [this]() { this->OnMenuItemPasteClicked(); });
+    menuInfoEdit.AddItem("Select All", [this]() { this->OnMenuItemSelectAllClicked(); });
 
-    menuInfoView.AddItem("Clear Log",   [this] () { this->OnMenuItemClearLogClicked(); });
+    menuInfoView.AddItem("Clear Log", [this]() { this->OnMenuItemClearLogClicked(); });
 
-    menuInfoMidi.AddItem("Initialize",  [this] () { this->OnMenuItemMidiInitialize(); });
+    menuInfoMidi.AddItem("Initialize", [this]() { this->OnMenuItemMidiInitialize(); });
 
     std::vector<MenuInfo> menuInfo;
     menuInfo.push_back(menuInfoFile);
@@ -119,17 +119,17 @@ void Program::InitializeMenu()
 
 void Program::CreateCallbacks()
 {
-    buttonCompile->onClicked += [this] (gpointer data) { this->OnButtonCompileClicked(data); };
-    buttonStop->onClicked += [this] (gpointer data) { this->OnButtonStopClicked(data); };
-    drawingarea1->onDraw += [this] (GtkWidget* widget, cairo_t* cr, gpointer data) { this->OnDrawWave(widget, cr, data); };
-    drawingarea2->onDraw += [this] (GtkWidget* widget, cairo_t* cr, gpointer data) { this->OnDrawLevels(widget, cr, data); };
-    drawingarea1->onButtonPressed += [this] (guint button, double x, double y) { this->OnDrawingAreaClicked(button, x, y); };
-    window->onClosing += [this] () { this->OnApplicationQuit(); };
+    buttonCompile->onClicked += [this](gpointer data) { this->OnButtonCompileClicked(data); };
+    buttonStop->onClicked += [this](gpointer data) { this->OnButtonStopClicked(data); };
+    drawingarea1->onDraw += [this](GtkWidget *widget, cairo_t *cr, gpointer data) { this->OnDrawWave(widget, cr, data); };
+    drawingarea2->onDraw += [this](GtkWidget *widget, cairo_t *cr, gpointer data) { this->OnDrawLevels(widget, cr, data); };
+    drawingarea1->onButtonPressed += [this](guint button, double x, double y) { this->OnDrawingAreaClicked(button, x, y); };
+    window->onClosing += [this]() { this->OnApplicationQuit(); };
 }
 
-void Program::LogMessage(const std::string& message)
+void Program::LogMessage(const std::string &message)
 {
-    if(IsClosing()) //prevents setting text on a destroyed text buffer
+    if (IsClosing()) //prevents setting text on a destroyed text buffer
         return;
     textviewLog->WriteLine(message);
 }
@@ -137,10 +137,10 @@ void Program::LogMessage(const std::string& message)
 void Program::OnButtonCompileClicked(gpointer data)
 {
     auto sourceview = tabcontrol->GetSelectedItem();
-    if(sourceview != nullptr)
+    if (sourceview != nullptr)
     {
         std::string source = sourceview->GetText();
-        if(SharpAudioAPI::Compile(source))
+        if (SharpAudioAPI::Compile(source))
         {
             SetInspectorFields();
         }
@@ -152,7 +152,7 @@ void Program::OnButtonStopClicked(gpointer data)
     SharpAudioAPI::StopScript();
 }
 
-void Program::OnDrawWave(GtkWidget* widget, cairo_t* cr, gpointer data)
+void Program::OnDrawWave(GtkWidget *widget, cairo_t *cr, gpointer data)
 {
     cairo_set_source_rgb(cr, backgroundColor.r, backgroundColor.g, backgroundColor.b);
     cairo_rectangle(cr, 0, 0, 128, 64);
@@ -161,38 +161,38 @@ void Program::OnDrawWave(GtkWidget* widget, cairo_t* cr, gpointer data)
     cairo_set_source_rgba(cr, foregroundColor.r, foregroundColor.g, foregroundColor.b, foregroundColor.a);
 
     int count = SharpAudioAPI::GetAudioSourceCount();
-  
-    if(count > 0)
+
+    if (count > 0)
     {
-        if(SharpAudioAPI::IsAudioSourcePlaying(0))
+        if (SharpAudioAPI::IsAudioSourcePlaying(0))
         {
-            int pos = SharpAudioAPI::GetPlaybackPosition(0);            
+            int pos = SharpAudioAPI::GetPlaybackPosition(0);
             long length = SharpAudioAPI::GetDataLength(0);
             long index = pos;
-            if(index >= length)
+            if (index >= length)
             {
                 index = (index - length);
             }
 
             double y = SharpAudioAPI::GetSampleAtOffset(0, index);
             y = y * 64;
-			y += 32;
-            cairo_move_to (cr, 0, y);
+            y += 32;
+            cairo_move_to(cr, 0, y);
 
             for (int i = 0; i < 128; i++)
             {
-                if(index >= length)
+                if (index >= length)
                 {
                     index = (index - length);
                 }
 
                 y = SharpAudioAPI::GetSampleAtOffset(0, index);
                 y = y * 64;
-				y += 32;
+                y += 32;
                 cairo_line_to(cr, i, y);
                 index++;
-            }    
-            
+            }
+
             cairo_stroke(cr);
         }
         else
@@ -212,20 +212,20 @@ void Program::OnDrawWave(GtkWidget* widget, cairo_t* cr, gpointer data)
     gtk_widget_queue_draw(widget);
 }
 
-void Program::OnDrawLevels(GtkWidget* widget, cairo_t* cr, gpointer data)
-{    
+void Program::OnDrawLevels(GtkWidget *widget, cairo_t *cr, gpointer data)
+{
     cairo_set_source_rgb(cr, foregroundColor.r, foregroundColor.g, foregroundColor.b);
 
-	int count = SharpAudioAPI::GetAudioSourceCount();
-	if(count > 0)
-	{
-		float left = SharpAudioAPI::GetSampleLeft(0);
-		float right = SharpAudioAPI::GetSampleRight(0);
-		cairo_rectangle(cr, 0, 0, left * 128, 31);
-		cairo_fill(cr);
-		cairo_rectangle(cr, 0, 32, right * 128, 31);
-		cairo_fill(cr);
-	}
+    int count = SharpAudioAPI::GetAudioSourceCount();
+    if (count > 0)
+    {
+        float left = SharpAudioAPI::GetSampleLeft(0);
+        float right = SharpAudioAPI::GetSampleRight(0);
+        cairo_rectangle(cr, 0, 0, left * 128, 31);
+        cairo_fill(cr);
+        cairo_rectangle(cr, 0, 32, right * 128, 31);
+        cairo_fill(cr);
+    }
 
     gtk_widget_queue_draw(widget);
 }
@@ -234,28 +234,28 @@ void Program::OnDrawingAreaClicked(guint button, double x, double y)
 {
     ColorPickerDialog dialog(window.get());
 
-    if(dialog.ShowDialog(window.get()) == DialogResult::OK)
+    if (dialog.ShowDialog(window.get()) == DialogResult::OK)
     {
         foregroundColor = dialog.GetColor();
     }
 }
 
 void Program::OnMenuItemOpenClicked()
-{    
+{
     OpenFileDialog dialog;
 
-    if(dialog.ShowDialog(window.get()) == DialogResult::OK)
+    if (dialog.ShowDialog(window.get()) == DialogResult::OK)
     {
         std::string filename = dialog.GetFileName();
-        
-        if(IO::FileExists(filename))
+
+        if (IO::FileExists(filename))
         {
-            std::string text = IO::ReadAllText(filename);            
+            std::string text = IO::ReadAllText(filename);
             auto tabPage = tabcontrol->AddItem();
-            
-            std::string tabname = StringUtility::GetFileNameWithExtension(filename);           
-            
-            if(tabPage != nullptr)
+
+            std::string tabname = StringUtility::GetFileNameWithExtension(filename);
+
+            if (tabPage != nullptr)
             {
                 tabPage->item->SetText(text);
                 tabPage->button->SetText(tabname);
@@ -268,17 +268,17 @@ void Program::OnMenuItemSaveClicked()
 {
     SaveFileDialog dialog;
 
-    if(dialog.ShowDialog(window.get()) == DialogResult::OK)
+    if (dialog.ShowDialog(window.get()) == DialogResult::OK)
     {
         std::string filename = dialog.GetFileName();
 
         int selectedTab = tabcontrol->GetSelectedIndex();
 
-        if(selectedTab >= 0)
+        if (selectedTab >= 0)
         {
             auto selected = tabcontrol->GetItemAtIndex(selectedTab);
 
-            if(selected != nullptr)
+            if (selected != nullptr)
             {
                 std::string text = selected->item->GetText();
                 IO::WriteAllText(text, filename);
@@ -297,11 +297,11 @@ void Program::OnMenuItemUndoClicked()
 {
     auto sourceview = tabcontrol->GetSelectedItem();
 
-    if(sourceview != nullptr)
-    {    
-        GtkSourceBuffer* buffer = sourceview->GetBuffer();
-        
-        if(gtk_source_buffer_can_undo(buffer))
+    if (sourceview != nullptr)
+    {
+        GtkSourceBuffer *buffer = sourceview->GetBuffer();
+
+        if (gtk_source_buffer_can_undo(buffer))
             gtk_source_buffer_undo(buffer);
     }
 }
@@ -310,39 +310,36 @@ void Program::OnMenuItemRedoClicked()
 {
     auto sourceview = tabcontrol->GetSelectedItem();
 
-    if(sourceview != nullptr)
-    {    
-        GtkSourceBuffer* buffer = sourceview->GetBuffer();
-        
-        if(gtk_source_buffer_can_redo(buffer))
+    if (sourceview != nullptr)
+    {
+        GtkSourceBuffer *buffer = sourceview->GetBuffer();
+
+        if (gtk_source_buffer_can_redo(buffer))
             gtk_source_buffer_redo(buffer);
     }
 }
 
 void Program::OnMenuItemCutClicked()
 {
-
 }
 
 void Program::OnMenuItemCopyClicked()
 {
-
 }
 
 void Program::OnMenuItemPasteClicked()
 {
-
 }
 
 void Program::OnMenuItemSelectAllClicked()
 {
     auto sourceview = tabcontrol->GetSelectedItem();
 
-    if(sourceview != nullptr)
+    if (sourceview != nullptr)
     {
-        GtkSourceBuffer* buffer = sourceview->GetBuffer();        
+        GtkSourceBuffer *buffer = sourceview->GetBuffer();
         GtkTextIter start, end;
-	    gtk_text_buffer_get_bounds(GTK_TEXT_BUFFER(buffer), &start, &end);        
+        gtk_text_buffer_get_bounds(GTK_TEXT_BUFFER(buffer), &start, &end);
         gtk_text_buffer_get_iter_at_offset(GTK_TEXT_BUFFER(buffer), &start, 0);
         gtk_text_buffer_get_iter_at_offset(GTK_TEXT_BUFFER(buffer), &end, 0);
         gtk_text_iter_forward_to_end(&end);
@@ -375,100 +372,100 @@ void Program::OnApplicationQuit()
 }
 
 void Program::SetInspectorFields()
-{	
+{
     int numberOfFields = SharpAudioAPI::GetFieldCount();
 
-    if(inspectorFields.size() > 0)
+    if (inspectorFields.size() > 0)
     {
-        for(size_t i = 0; i < inspectorFields.size(); i++)
+        for (size_t i = 0; i < inspectorFields.size(); i++)
         {
             inspectorFields[i].Destroy();
         }
     }
 
-    inspectorFields.clear();    
+    inspectorFields.clear();
 
-    for(int i = 0; i < numberOfFields; i++)
-    {		
-        int index = i;        
+    for (int i = 0; i < numberOfFields; i++)
+    {
+        int index = i;
         int fieldType = SharpAudioAPI::GetFieldType(index);
-        
+
         std::string fieldName;
-        SharpAudioAPI::GetFieldName(index, fieldName);         
-        
+        SharpAudioAPI::GetFieldName(index, fieldName);
+
         std::string fieldValue;
         SharpAudioAPI::GetFieldValue(index, fieldValue);
 
-		double fieldMinValue = SharpAudioAPI::GetFieldMinValue(index);
-		double fieldMaxValue = SharpAudioAPI::GetFieldMinValue(index);
+        double fieldMinValue = SharpAudioAPI::GetFieldMinValue(index);
+        double fieldMaxValue = SharpAudioAPI::GetFieldMinValue(index);
 
-        InspectorField field;		
-		field.SetValueRange(fieldMinValue, fieldMaxValue);
+        InspectorField field;
+        field.SetValueRange(fieldMinValue, fieldMaxValue);
         field.Create((FieldType)fieldType, panel3->widget, fieldName, index);
         FieldType ft = (FieldType)fieldType;
         field.Show();
         field.SetValue(fieldValue);
-        inspectorFields.push_back(field);    
+        inspectorFields.push_back(field);
     }
 
-    for(int i = 0; i < numberOfFields; i++)
+    for (int i = 0; i < numberOfFields; i++)
     {
         gpointer id = (gpointer)(&inspectorFields[i].identifier);
 
-		switch(inspectorFields[i].type)
-		{
-			case FieldType::TypeBool:
-			{
-				g_signal_connect(GTK_TOGGLE_BUTTON(inspectorFields[i].inputWidget), "toggled", G_CALLBACK(OnCheckButtonToggled), id);
-				break;
-			}
-			case FieldType::TypeDouble:
-			{
-				g_signal_connect(GTK_SPIN_BUTTON(inspectorFields[i].inputWidget), "value-changed", G_CALLBACK(OnSpinButtonValueChanged), id);
-				break;
-			}
-			case FieldType::TypeFloat:
-			{
-				g_signal_connect(GTK_SPIN_BUTTON(inspectorFields[i].inputWidget), "value-changed", G_CALLBACK(OnSpinButtonValueChanged), id);
-				break;
-			}        
-			case FieldType::TypeInt:
-			{
-				g_signal_connect(GTK_SPIN_BUTTON(inspectorFields[i].inputWidget), "value-changed", G_CALLBACK(OnSpinButtonValueChanged), id);
-				break;
-			}        
-			case FieldType::TypeString:
-			{
-				g_signal_connect(GTK_ENTRY(inspectorFields[i].inputWidget), "changed", G_CALLBACK(OnEntryTextChanged), id);
-				break;
-			}
-		}        
-    }	
+        switch (inspectorFields[i].type)
+        {
+            case FieldType::TypeBool:
+            {
+                g_signal_connect(GTK_TOGGLE_BUTTON(inspectorFields[i].inputWidget), "toggled", G_CALLBACK(OnCheckButtonToggled), id);
+                break;
+            }
+            case FieldType::TypeDouble:
+            {
+                g_signal_connect(GTK_SPIN_BUTTON(inspectorFields[i].inputWidget), "value-changed", G_CALLBACK(OnSpinButtonValueChanged), id);
+                break;
+            }
+            case FieldType::TypeFloat:
+            {
+                g_signal_connect(GTK_SPIN_BUTTON(inspectorFields[i].inputWidget), "value-changed", G_CALLBACK(OnSpinButtonValueChanged), id);
+                break;
+            }
+            case FieldType::TypeInt:
+            {
+                g_signal_connect(GTK_SPIN_BUTTON(inspectorFields[i].inputWidget), "value-changed", G_CALLBACK(OnSpinButtonValueChanged), id);
+                break;
+            }
+            case FieldType::TypeString:
+            {
+                g_signal_connect(GTK_ENTRY(inspectorFields[i].inputWidget), "changed", G_CALLBACK(OnEntryTextChanged), id);
+                break;
+            }
+        }
+    }
 }
 
 void Program::OnCheckButtonToggled(GtkToggleButton *widget, gpointer user_data)
 {
-    FieldIdentifier *identifier = (FieldIdentifier*)user_data;
+    FieldIdentifier *identifier = (FieldIdentifier *)user_data;
     int id = identifier->id;
     std::string newValue = inspectorFields[id].GetValue();
 
-	SharpAudioAPI::SetFieldValue(id, newValue);
+    SharpAudioAPI::SetFieldValue(id, newValue);
 }
 
 void Program::OnEntryTextChanged(GtkEditable *editable, gpointer user_data)
 {
-    FieldIdentifier *identifier = (FieldIdentifier*)user_data;
+    FieldIdentifier *identifier = (FieldIdentifier *)user_data;
     int id = identifier->id;
     std::string newValue = inspectorFields[id].GetValue();
-    
-	SharpAudioAPI::SetFieldValue(id, newValue);
+
+    SharpAudioAPI::SetFieldValue(id, newValue);
 }
 
 void Program::OnSpinButtonValueChanged(GtkSpinButton *spin_button, gpointer user_data)
 {
-    FieldIdentifier *identifier = (FieldIdentifier*)user_data;
+    FieldIdentifier *identifier = (FieldIdentifier *)user_data;
     int id = identifier->id;
     std::string newValue = inspectorFields[id].GetValue();
 
-	SharpAudioAPI::SetFieldValue(id, newValue);
+    SharpAudioAPI::SetFieldValue(id, newValue);
 }
